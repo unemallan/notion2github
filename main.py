5ab50a55ef4c57c3487d83289e6435a6a828d8db
unemallan <umakaran.nemallan@gmail.com>
2024-07-09 18:15:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+
+import os
+from narkdown.exporter import NotionExporter
 from narkdown.cli import main
 
 if __name__ == "__main__":
+    token = os.environ['NOTION_TOKEN']
+    page_url = "https://www.notion.so/c192c7104ab341f8ae581d0697419986"
+    database_url = "https://www.notion.so/c192c7104ab341f8ae581d0697419986?v=7962af9b51ff41f69f9ecc5e1dbef96d"
+
+    notion_exporter = NotionExporter(token)
+    notion_exporter.get_notion_page(page_url)
+    notion_exporter.get_notion_pages_from_database(database_url)
+
     main()
