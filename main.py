5a5d58526a557b02637ac98887b6688b6fabc0a4
unemallan <umakaran.nemallan@gmail.com>
2024-07-09 19:00:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,5 @@ from narkdown.exporter import NotionExporter
 
 if __name__ == "__main__":
     token = "v02%3Auser_token_or_cookies%3AqrGl6ptb4FaRQVAiazKOUol_zkTOkIeVyapBEtd7JeESoLvedsv8sQY-tXHAhfc5_Toq6b5IkXy3kzhEDszm0MfNoBOa1M2nz0Yq1tuW8iXISYQMsmgVXX7Dr6_vi0NZVfi6"
-    page_url = "https://www.notion.so/c192c7104ab341f8ae581d0697419986"
-
     notion_exporter = NotionExporter(token)
-    notion_exporter.get_notion_page(page_url)
     main()
